1e4d5cafa9aa773b4e2ba8b5ac2774b3d2a6cb11
jjchowaniec1 <jjchowaniec@buffs.wtamu.edu>
2021-11-28 15:13:22 -0500

    google analytics

--- blog_deploy/settings.py
+++ blog_deploy/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
 if settings.DEBUG:
     ALLOWED_HOSTS = ['*']
